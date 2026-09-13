266091789b24868ae38bd1a8800c2ed282f024a2
Tanmoy9088 <tanmoyss007@gmail.com>
2026-09-13 12:56:59 +0530

    fix: change the siteurl link

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://skillswap.vercel.app";
+const siteUrl = "https://skillswap-mu-pied.vercel.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
